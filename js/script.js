836dcbdb9c7281a224671fb65c77ce50de457bdf
VolodymyrUkr <vladimir3v@gmail.com>
2021-01-27 21:53:50 +0200

    first animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 window.onload = function() {
-    document.getElementById("typewriter-input")
+    var i = 0;
+    var txt = 'Wake up, Neo...'; 
+    var speed = 75;
+    
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("typewriter-input").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+    typeWriter();
 }
